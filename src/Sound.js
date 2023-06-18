aa2b1cd6ba6c24ea6684e6a50c84dda0a35b0d38
Magdalena Sulkowska <magdalena.sulkowska@aptiv.com>
2023-06-18 13:18:22 +0200

    check

--- src/Sound.js
+++ src/Sound.js
@@ -1,28 +1,29 @@
 import "./App.css";
 import morse from "./morse.wav";
-import {  useEffect } from "react";
+import { useEffect } from "react";
 import speaker from "./speaker.png";
 import { useGlobalAudioPlayer } from "react-use-audio-player";
 
 function Sound() {
+  console.log("aaa");
   const { load } = useGlobalAudioPlayer();
 
-  useEffect(()=>{
+  useEffect(() => {
     load(morse, {
-        autoplay: true,
-        loop: true,
-      });
-  },[load])
+      autoplay: true,
+      loop: true,
+    });
+  }, [load]);
 
   return (
-        <div id="speaker-wrapper">
-          <img
-            src={speaker}
-            alt="speaker"
-            id="speaker"
-            onDragStart={(e) => e.preventDefault()}
-          />
-        </div>
+    <div id="speaker-wrapper">
+      <img
+        src={speaker}
+        alt="speaker"
+        id="speaker"
+        onDragStart={(e) => e.preventDefault()}
+      />
+    </div>
   );
 }
 
