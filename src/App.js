806ac03353f9cf3d60e7cd50b058dc76b91053cd
Magdalena Sulkowska <magdalena.sulkowska@aptiv.com>
2023-06-18 16:14:51 +0200

    simplification

--- src/App.js
+++ src/App.js
@@ -1,74 +1,29 @@
 import "./App.css";
-import { useState, useEffect, useCallback } from "react";
-import raccoon from "./fat-raccoon.png";
-import Sound from "./Sound";
+import { useState } from "react";
+import morse from "./morse.wav";
+import volume from "./volume.png";
+import play from "./play.png"
 
 function App() {
-  const [playSound, setPlaySound] = useState(0);
-  const [top, setTop] = useState(0);
-  const [left, setLeft] = useState(0);
-
-  const longPress = useLongPress(() => {
-    if (playSound === 0) {
-      setPlaySound(1);
-    }
-  }, 2000);
+  const [isPlaying, setIsPlaying] = useState(false);
+  let audio = new Audio(morse);
+  audio.loop=true;
 
   return (
-    <div>
-      {playSound === 0 && (
-        <img
-          src={raccoon}
-          alt="raccoon"
-          id="raccoon"
-          style={{ top: top + "px", left: left + "px" }}
-          {...longPress}
-          onClick={() => {
-            setTop(Math.floor(Math.random() * (window.innerHeight - 186)));
-            setLeft(Math.floor(Math.random() * (window.innerWidth - 252)));
-          }}
-          onContextMenu={(e)=>{
-            e.preventDefault();
-            e.stopPropagation();
-            return false;
-          }}
-        />
-      )}
-      {playSound === 1 && <Sound />}
+    <div className="wrapper">
+      <img
+        src={isPlaying ? volume : play}
+        alt="m"
+        className="speaker"
+        onClick={() => {
+          if (!isPlaying) {
+            setIsPlaying(true);
+            audio.play();
+          }
+        }}
+      />
     </div>
   );
 }
 
 export default App;
-
-function useLongPress(callback = () => {}, ms = 300) {
-  const [startLongPress, setStartLongPress] = useState(false);
-
-  useEffect(() => {
-    let timerId;
-    if (startLongPress) {
-      timerId = setTimeout(callback, ms);
-    } else {
-      clearTimeout(timerId);
-    }
-
-    return () => {
-      clearTimeout(timerId);
-    };
-  }, [callback, ms, startLongPress]);
-
-  const start = useCallback(() => {
-    setStartLongPress(true);
-  }, []);
-  const stop = useCallback(() => {
-    setStartLongPress(false);
-  }, []);
-
-  return {
-    onMouseDown: start,
-    onMouseUp: stop,
-    onMouseLeave: stop,
-    onTouchStart: start,
-    onTouchEnd: stop,
-  };
-}
